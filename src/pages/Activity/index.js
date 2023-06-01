5416ec154b350b7ebaa06312927388037aca434b
Le Khac Anh Dai <khacdai0801@gmail.com>
2023-06-01 23:58:31 +0700

    revse 2:

--- src/pages/Activity/index.js
+++ src/pages/Activity/index.js
@@ -1,12 +1,5 @@
-import {Card, Table, Button, Avatar, Typography, Tag, Progress} from 'antd';
+import {Card, Table, Button, Typography, Tag, Progress} from 'antd';
 
-// Images
-import face from '../../assets/images/face-1.jpg';
-import face2 from '../../assets/images/face-2.jpg';
-import face3 from '../../assets/images/face-3.jpg';
-import face4 from '../../assets/images/face-4.jpg';
-import face5 from '../../assets/images/face-5.jpeg';
-import face6 from '../../assets/images/face-6.jpeg';
 import {Link, useHistory} from 'react-router-dom';
 import SITE_MAP from '../../constants/path';
 import {
